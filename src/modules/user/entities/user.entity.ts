212c8070537298aa278d06b8b120a5bc27ca9eee
NickRayan <nickcerron@gmail.com>
2024-07-12 12:32:12 -0500

    user: entity defined

--- src/modules/user/entities/user.entity.ts
+++ src/modules/user/entities/user.entity.ts
@@ -1,2 +1,49 @@
-@Entity
-export class User {}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({
+    type: 'varchar',
+    nullable: true,
+    length: 100,
+  })
+  name: string;
+
+  @Column({
+    type: 'varchar',
+    nullable: true,
+    length: 100,
+  })
+  lastname: string;
+
+  @Column({
+    type: 'varchar',
+    nullable: true,
+    length: 50,
+  })
+  username: string;
+
+  @Column({
+    type: 'varchar',
+    nullable: true,
+    length: 200,
+  })
+  email: string;
+
+  @Column({
+    type: 'varchar',
+    nullable: false,
+    length: 100,
+  })
+  password: string;
+
+  @Column({
+    type: 'boolean',
+    nullable: false,
+    default: true,
+  })
+  isActive: boolean;
+}
